23bf2a5e77bafeb29321246d8719f0ee218c8ebf
genga <genga898@gmail.com>
2026-01-21 10:52:10 +0300

    update: commit latest config

--- hypr/start.sh
+++ hypr/start.sh
@@ -9,16 +9,14 @@ nm-applet --indicator&
 #blueman-applet: bluetooth
 blueman-applet &
 
-#eww: topbar
-eww daemon&
-eww open topbar &
-eww open notification_popup_window &
+#eww
+eww open-many topbar notification_popup_window &
 
 # swayosd-server: osd(on-screen-display)
 swayosd-server &
 
 # walker app laucner
-walker --gapplication-service &
+vicinae server &
 
 # eww:custom notification daemon
 /usr/bin/notification-daemon &
